0ee754560bbefd2c8552054c6d6afbb7f61f6b89
hoantran-ABK <hoan.tran@activision.com>
2022-07-20 14:37:33 -0700

    initial submission chal1, chal2
    intial submissions for challenge 1 and challenge 2 for ch3, leap year detector and factorize number

--- cgspectrum_ch3_challenges/cgspectrum_ch3_challenge1/challenge1.cpp
+++ cgspectrum_ch3_challenges/cgspectrum_ch3_challenge1/challenge1.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
-
+#include <chrono>
+#include <ctime>
+#include <string>
 
 int main() {
+	int year;
+	char t_buff[10];
+	struct tm local_tm;
 
+	// get current year from system clock
+	std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
+	time_t tt = std::chrono::system_clock::to_time_t(tp);
+	localtime_s(&local_tm, &tt);
+	strftime(t_buff, 10, "%Y", &local_tm);
+	year = std::stoi(t_buff);
+
+	if (year % 4 == 0) {
+		// leap year...
+		//UNLESS
+		if (year % 100 == 0) {
+			// not leap year...
+			//UNLESS
+			if (year % 400 == 0) {
+				// leap year...
+				std::cout << year << " is a Leap year...\n";
+			}
+			else {
+				std::cout << year << " is not a Leap year...\n";
+			}
+		}
+		else {
+			std::cout << year << " is a Leap year...\n";
+		}
+	}
+	else {
+		std::cout << year << " is not a Leap year...\n";
+	}
 }
--- cgspectrum_ch3_challenges/cgspectrum_ch3_challenge2/challenge2.cpp
+++ cgspectrum_ch3_challenges/cgspectrum_ch3_challenge2/challenge2.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
-
+#include <vector>
+#include <set>
+#include <numeric>
 
 int main() {
+	int response;
+	int max_factor;
 
+	std::vector<int> p_factors;
+	std::set<int> results;
+	std::cout << "Provide a number value: ";
+	std::cin >> response;
+
+	max_factor = response;
+
+	results.insert(1);
+	results.insert(response);
+	// [2, 3, 5, 7] prime factorizations
+	while (true) {
+		if (response % 2 == 0 && response != 2) {
+			response = response / 2;
+			p_factors.push_back(2);
+			continue;
+		}
+		else if (response % 3 == 0 && response != 3) {
+			response = response / 3;
+			p_factors.push_back(3);
+			continue;
+		}
+		else if (response % 5 == 0 && response != 5) {
+			response = response / 5;
+			p_factors.push_back(5);
+			continue;
+		}
+		else if (response % 7 == 0 && response != 7) {
+			response = response / 7;
+			p_factors.push_back(7);
+			continue;
+		}
+		else {
+			// no more prime values to factor out
+			p_factors.push_back(response);
+			break;
+		}
+	}
+
+	for (int offset = 0; offset < p_factors.size(); offset++) {
+		for (int i = 0; i+offset < p_factors.size(); i++) {
+			int factor = std::accumulate(p_factors.begin()+i, p_factors.begin()+i+offset+1, 1, std::multiplies<>());
+			results.insert(factor);
+		}
+	}
+
+	for ( int fact : results) {
+		std::cout << fact << std::endl;
+	}
 }
